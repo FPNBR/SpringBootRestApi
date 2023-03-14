be8becff105477ed021a37a00b54e643b8b3a7c5
FPNBR <felipepny@gmail.com>
2023-03-14 11:25:25 -0300

    Criando o model e tabela no banco

--- src/main/java/br/com/fpnbr/springbootrestapi/controllers/GreetingsController.java
+++ src/main/java/br/com/fpnbr/springbootrestapi/controllers/GreetingsController.java
@@ -10,4 +10,10 @@ public class GreetingsController {
     public String greetingText(@PathVariable String name) {
         return  "Hello " + name + "!";
     }
+
+    @RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String retornaOlaMundo(@PathVariable String nome) {
+        return "Olá mundo " + nome;
+    }
 }
