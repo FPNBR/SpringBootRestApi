ad53548c1af78d6ca7721fbd5884916ad1696e43
FPNBR <felipepny@gmail.com>
2023-03-14 12:32:31 -0300

    Criando END-POINT de listar todos usuários

--- src/main/java/br/com/fpnbr/springbootrestapi/controllers/SpringBootRestApiController.java
+++ src/main/java/br/com/fpnbr/springbootrestapi/controllers/SpringBootRestApiController.java
@@ -4,7 +4,10 @@ import br.com.fpnbr.springbootrestapi.models.Usuario;
 import br.com.fpnbr.springbootrestapi.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class SpringBootRestApiController {
@@ -27,5 +30,13 @@ public class SpringBootRestApiController {
         usuarioRepository.save(usuario);
 
         return "Olá mundo " + nome;
+    }
+
+    @GetMapping(value = "listarUsuarios")
+    @ResponseBody // Retorna os dados para ao corpo da requisição
+    public ResponseEntity<List<Usuario>> usuarioList() {
+        List<Usuario> usuarioList = usuarioRepository.findAll(); // Executa a consulta no banco de dados
+
+        return new ResponseEntity<>(usuarioList, HttpStatus.OK); // Retorna a lista em JSON
     }
 }
