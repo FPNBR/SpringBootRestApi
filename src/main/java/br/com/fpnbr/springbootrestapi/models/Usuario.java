3910122fe56781dc4fbf0aa2427e2b38565206d9
FPNBR <felipepny@gmail.com>
2023-03-14 15:38:52 -0300

    Definindo o contexto da aplicação

--- src/main/java/br/com/fpnbr/springbootrestapi/models/Usuario.java
+++ src/main/java/br/com/fpnbr/springbootrestapi/models/Usuario.java
@@ -1,9 +1,14 @@
 package br.com.fpnbr.springbootrestapi.models;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.io.Serializable;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @SequenceGenerator(name = "seq_usuario", sequenceName = "seq_usuario", allocationSize = 1, initialValue = 1)
 public class Usuario implements Serializable {
@@ -15,28 +20,4 @@ public class Usuario implements Serializable {
     private String nome;
 
     private int idade;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
